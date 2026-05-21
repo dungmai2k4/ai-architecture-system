d44dca596d7a1141784d58fbc2bf2fe437d22eec
Mai Tien Dung <dungmai2k4@gmail.com>
2026-05-22 00:55:00 +0700

    Initiating project

---
+++ backend/src/main/java/com/architectai/ArchitectAiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.architectai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitectAiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchitectAiBackendApplication.class, args);
+	}
+
+}
