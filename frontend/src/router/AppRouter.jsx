ee6ce8fafd741f0969d24b896a3f068a92bb3ad4
Mai Tien Dung <dungmai2k4@gmail.com>
2026-05-22 12:17:54 +0700

    update AppRouter

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -1,18 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import HomePage from '../pages/HomePage'
-import DesignPage from '../pages/DesignPage'
-import ResultPage from '../pages/ResultPage'
-import Viewer3DPage from '../pages/Viewer3DPage'
-
 function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/design" element={<DesignPage />} />
-        <Route path="/result" element={<ResultPage />} />
-        <Route path="/viewer" element={<Viewer3DPage />} />
+        {/* <Route path="/" element={<Home />} /> */}
       </Routes>
     </BrowserRouter>
   )
